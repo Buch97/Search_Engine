310a110a5c505456388d751182f0bb8da1171a9b
Buch97 <fpucciofilo97@gmail.com>
2022-11-07 18:15:34 +0100

    merge implementato e funzionante su un sample

--- src/main/java/it/unipi/Inverted_Index_Construction.java
+++ src/main/java/it/unipi/Inverted_Index_Construction.java
@@ -15,7 +15,6 @@ public class Inverted_Index_Construction {
     public final static int SPIMI_TOKEN_STREAM_MAX_LIMIT = 3000;
     public final static List<Token> tokenStream = new ArrayList<>();
     public static Map<Integer, Doc_Stats> documents = new HashMap<>();
-    public static Map<String, Term_Stats> vocabulary = new HashMap<>();
     public static int block_number = 0;
     public static File inverted_index = new File("C:\\Users\\pucci\\Desktop\\AIDE\\" +
             "Multimedia Information Retrieval and Computer Vision\\inverted_index.tsv");
@@ -31,22 +30,22 @@ public class Inverted_Index_Construction {
                 String[] row = data.split("\t");
                 String doc_no = row[0];
                 String text = row[1];
+
                 documentIndexMapping(doc_no, text);
+
                 parseDocumentBody(Integer.parseInt(doc_no), text);
-                mergeBlocks();
             }
 
-            for (Token elem : tokenStream)
-                System.out.println(elem.getTerm() + ":" + elem.getDoc_id() + ":" + elem.getFrequency());
-
             Document_Index_Hash document_index = new Document_Index_Hash(documents);
-            Lexicon lexicon = new Lexicon(vocabulary);
-            //lexicon.print();
             //document_index.print();
             myReader.close();
+            mergeBlocks();
+
 
         } catch (FileNotFoundException e) {
             System.out.println("An error occurred.");
+            e.printStackTrace();
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
@@ -64,7 +63,6 @@ public class Inverted_Index_Construction {
             tokenStream.add(new Token(token, tokenizer.getDocId(), results.get(token)));
 
         if (tokenStream.size() >= SPIMI_TOKEN_STREAM_MAX_LIMIT) {
-            // Call SPIMI once the block is full.
             invertedIndexSPIMI();
             tokenStream.clear();
         }
@@ -97,10 +95,10 @@ public class Inverted_Index_Construction {
             FileWriter myWriter = new FileWriter(output_file);
 
             for (String term : vocabulary.keySet()) {
-                myWriter.write(term);
+                myWriter.write(term + "\t");
 
                 for (Posting p : vocabulary.get(term).getPostingList())
-                    myWriter.write("\t" + p.getDoc_id() + ":" + p.getTerm_frequency());
+                    myWriter.write(p.getDoc_id() + ":" + p.getTerm_frequency() + " ");
 
                 myWriter.write("\n");
             }
@@ -121,16 +119,85 @@ public class Inverted_Index_Construction {
         return postings_list;
     }
 
-    private static void mergeBlocks() throws FileNotFoundException {
-        final List<BufferedReader> readerList = new ArrayList<BufferedReader>();
+    // apro tutti i file in parallelo, ad ogni iterazione devo prendere il termine alfabeticamente minore fra tutti
+    // andare a vedere in tutti gli altri file se contengono quel termine e concatenare le posting lists
+    // parto leggendo la prima riga di tutti i file, prendo il termine minore e faccio la procedura, avanzo di una riga
+    // solo sui file da cui ho letto il termine corrente
 
-        for(int i = 0 ; i < block_number ; i++){
+    private static void mergeBlocks() throws IOException {
+        ArrayList<String> orderedLines = new ArrayList<>();
+        List<BufferedReader> readerList = new ArrayList<>();
+        BufferedWriter output = new BufferedWriter(new FileWriter("C:\\Users\\pucci\\Desktop\\AIDE\\" +
+                "Multimedia Information Retrieval and Computer Vision\\output.tsv"));
+
+        for(int i = 1 ; i <= block_number ; i++){
             readerList.add(new BufferedReader(new FileReader("C:\\Users\\pucci\\Desktop\\AIDE\\" +
                     "Multimedia Information Retrieval and Computer Vision\\inverted_index" + i + ".tsv")));
+        }
 
+        ArrayList<String> currentReadedLines = new ArrayList<>();
+        //metto la prima riga di ogni file dentro due arraylist: orderedLines e currentReadedLines (mi serve per associare uno
+        // specifico reader alla posting da lui letta. Perche orderedLines viene poi ordinato e perdo traccia di questa
+        // informazione perche l indice nell arrayList non corrisponde piu allo specifico reader)
+        for (BufferedReader reader : readerList) {
+            String line = reader.readLine();
+            if (line != null) {
+                currentReadedLines.add(line);
+                orderedLines.add(line);
+            }
+            else
+                reader.close();
+        }
 
+        // condizione di uscita dal loop infinito (quando tutti i reader sono arrivati a EOF e quindi l'arraylist avrà
+        // size 0 perche non ci viene inserito nessun elemento)
+        while (orderedLines.size() != 0) {
+
+            //ordino l'array e prendo il primo elemento (che sarà il minore alfabeticamente)
+            Collections.sort(orderedLines);
+            String currentTerm = orderedLines.get(0).split("\t")[0];
+            //System.out.println("MINORE: " + currentTerm);
+
+            for (String elem : orderedLines)
+                System.out.println("orderedLines " + elem);
+
+            output.write(currentTerm + "\t");
+
+            for (String row : orderedLines) {
+                String term = row.split("\t")[0];
+                String posting = row.split("\t")[1];
+
+                if (Objects.equals(term, currentTerm))
+                    output.append(posting);
+            }
+
+            output.write("\n");
+
+            //rimuovo le righe appena processate dal mio array di appoggio
+            orderedLines.removeIf(elem -> elem.split("\t")[0].equals(currentTerm));
+
+            //guardo in quali file ho letto il currentTerm e solo in quelli avanzo il reader e metto la riga nuova che leggo nel mio orderedLines
+            for (String elem : currentReadedLines) {
+                if ((elem != null) && (Objects.equals(elem.split("\t")[0], currentTerm))) {
+                    int fileIndex = currentReadedLines.indexOf(elem);
+                    String nextRow = readerList.get(fileIndex).readLine();
+                    currentReadedLines.set(fileIndex, nextRow);
+                    if (nextRow != null)
+                        orderedLines.add(nextRow);
+                    else
+                        readerList.get(fileIndex).close();
+                }
+            }
+
+            /*for (String elem : currentReadedLines)
+                System.out.println("***Lines***: " + elem);
+
+            for (String elem : orderedLines)
+                System.out.println("***orderedLines***: " + elem);*/
 
         }
+
+        output.close();
 
     }
 
